d3840256a79a31e231b1b70969564d44b3a6bdae
Sina Sartip <ssartipz@ryerson.ca>
2021-01-19 03:38:59 +0000

    problem generator cleaner..need printer class

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -17,37 +17,68 @@ class arithmeticProblem():
         return str(self.value_1 + self.value_2)
       elif self.operand == '-':
         return str(self.value_1 - self.value_2)
+    else:
+      return ""
 
-def generate_problem(problem):
-  return arithmeticProblem(*problem.split())
+def generate_problem(problem, val1, val2, operand, error, ans):
+  split_problem = arithmeticProblem(*problem.split())
+
+  val1.append(split_problem.value_1)
+  val2.append(split_problem.value_2)
+  operand.append(split_problem.operand)
+  ans.append(split_problem.find_answer())
+  if split_problem.error_flag:
+    error.append(True)
+  else:
+    error.append(False)
+
+  return (val1, val2, operand, error, ans)
+
+def append_value(valueList, OutputList, adjust_side):
+  for value in valueList:
+    adjust = 4-len(str(value))
+    if adjust_side == 'left':
+      for _ in range(adjust):
+        OutputList.append(" ")  
+    
+    OutputList.append("    ")
+    OutputList.append(str(value))
+
+    if adjust_side == 'right':
+      for _ in range(adjust):
+        OutputList.append(" ")  
+
+  return OutputList
+  
 
 def arithmetic_arranger(problems, give_answer = False):
   #TODO : check the number of problems given
-  split_problems = []
+  val1 = []
+  val2 = []
+  operand = []
+  error = []
+  ans = []
   arranged_problemList = []
   arranged_problems = ""
 
   for problem in problems:
-    split_problems.append(generate_problem(problem))
+    val1,val2,operand,error,ans = generate_problem(problem, val1, val2, operand,                                                  error, ans)
 
-  
-  for problem in split_problems:
-    if problem.error_flag:
-      arranged_problemList.append(problem.check_operand())
-      break
-    arranged_problemList.append(str(problem.value_1))
+  if error.count(True) > 1:    
+    arranged_problemList.append(problem.check_operand())
+  else:
+    arranged_problemList = append_value(val1, arranged_problemList,'left')
     arranged_problemList.append("\n")
-    arranged_problemList.append(problem.operand)
+    arranged_problemList = append_value(operand, arranged_problemList,'right')
+    arranged_problemList = append_value(val2, arranged_problemList,'left')
     arranged_problemList.append("\n")
-    arranged_problemList.append(str(problem.value_2))
-    arranged_problemList.append("\n")
-
-    if give_answer:
+    for _ in range(len(val1)):
+      arranged_problemList.append("    ")
       arranged_problemList.append("-----")
-      arranged_problemList.append("\n")
-      arranged_problemList.append(problem.find_answer())
-      arranged_problemList.append("\n")
-  
+    arranged_problemList.append("\n")
+    if give_answer:
+      arranged_problemList = append_value(ans, arranged_problemList,'left')
+
   for math_string in arranged_problemList:
     arranged_problems += math_string
   print(arranged_problems)
